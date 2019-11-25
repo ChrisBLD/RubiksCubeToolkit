d61c7a6a4aa98818e06c68a9b75f831f70da12f9
cxm763 <cxm763@student.bham.ac.uk>
2019-11-25 19:06:49 +0000

    Individual moves working, need to make them work together though.

--- src/tutorial/RubiksCube.java
+++ src/tutorial/RubiksCube.java
@@ -1,13 +1,21 @@
 package tutorial;
 
+import java.awt.RenderingHints.Key;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Point3D;
 import javafx.scene.AmbientLight;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
@@ -15,7 +23,11 @@ import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -24,6 +36,7 @@ import javafx.scene.shape.TriangleMesh;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * Simple implementation of the Rubik's cube using JavaFX 3D
@@ -64,12 +77,33 @@ public class RubiksCube extends Application {
     public void start(Stage primaryStage) {
 
         Group sceneRoot = new Group();
-        SubScene subScene = new SubScene(sceneRoot, 600, 600, true, SceneAntialiasing.BALANCED);
-        subScene.setFill(Color.BLACK);
+        SubScene subScene = new SubScene(sceneRoot, 500, 500, true, SceneAntialiasing.BALANCED);
+        subScene.setFill(Color.SILVER);
+        Translate pivot = new Translate();
+        Rotate yRotate = new Rotate(0, Rotate.Y_AXIS);
         PerspectiveCamera camera = new PerspectiveCamera(true);
         camera.setNearClip(0.1);
         camera.setFarClip(10000.0);
-        camera.setTranslateZ(-10);
+        //camera.setTranslateZ(-15);
+        camera.getTransforms().addAll (
+                pivot,
+                yRotate,
+                new Rotate(-20, Rotate.X_AXIS),
+                new Translate(0, 0, -15)
+        );
+        
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(
+                Duration.seconds(0), 
+                new KeyValue(yRotate.angleProperty(), 0)
+        ));
+        timeline.getKeyFrames().add(new KeyFrame(
+                Duration.seconds(15), 
+                new KeyValue(yRotate.angleProperty(), 360)
+        ));
+        
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
         subScene.setCamera(camera);
 
         PhongMaterial mat = new PhongMaterial();
@@ -78,31 +112,39 @@ public class RubiksCube extends Application {
         Group meshGroup = new Group();
 
         AtomicInteger cont = new AtomicInteger();
-        patternFaceF.forEach(p -> {
-            MeshView meshP = new MeshView();
-            meshP.setMesh(createCube(p));
+        for (int i = 0; i < patternFaceF.size(); i++) {
+        	MeshView meshP = new MeshView();
+            meshP.setMesh(createCube(patternFaceF.get(i)));
             meshP.setMaterial(mat);
             Point3D pt = pointsFaceF.get(cont.getAndIncrement());
             meshP.getTransforms().addAll(new Translate(pt.getX(), pt.getY(), pt.getZ()));
             meshGroup.getChildren().add(meshP);
-        });
+            sceneRoot.getChildren().addAll(meshP, new AmbientLight(Color.WHITE));
+        }
+
+
         
-        Rotate rotateX = new Rotate(30, 0, 0, 0, Rotate.X_AXIS);
-        Rotate rotateY = new Rotate(20, 0, 0, 0, Rotate.Y_AXIS);
-        meshGroup.getTransforms().addAll(rotateX, rotateY);
-        
-        sceneRoot.getChildren().addAll(meshGroup, new AmbientLight(Color.WHITE));
-        
-        Box box = new Box(500,500,500);
-        sceneRoot.getChildren().add(box);
+       // sceneRoot.getChildren().addAll(meshGroup, new AmbientLight(Color.WHITE));
         
         BorderPane pane = new BorderPane();
         pane.setCenter(subScene);
         Button button = new Button("Reset");
         ToolBar toolBar = new ToolBar(button);
-        toolBar.setOrientation(Orientation.VERTICAL);
-        pane.setRight(toolBar);
-        pane.setPrefSize(400, 400);
+        toolBar.setOrientation(Orientation.HORIZONTAL);
+        toolBar.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        pane.setBottom(toolBar);
+        ToolBar toolBarRight = new ToolBar(button);
+        toolBarRight.setOrientation(Orientation.VERTICAL);
+        toolBarRight.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
+        pane.setRight(toolBarRight);
+        pane.setPrefSize(300, 300);
+        toolBar.setPrefWidth(100);
+        toolBar.setPrefHeight(100);
+        toolBarRight.setPrefWidth(100);
+        toolBarRight.setPrefHeight(700);
+
+        //Rotate rotateX = new Rotate(20, Rotate.X_AXIS);
+        //Rotate rotateY = new Rotate(15,Rotate.Y_AXIS);
         
         Scene scene = new Scene(pane);
         scene.setFill(Color.BLACK);
@@ -111,14 +153,21 @@ public class RubiksCube extends Application {
             mouseOldX = me.getSceneX();
             mouseOldY = me.getSceneY();
         });
-        scene.setOnMouseDragged(me -> {
+        /*scene.setOnMouseDragged(me -> {
 
             mousePosX = me.getSceneX();
             mousePosY = me.getSceneY();
             rotateX.setAngle(rotateX.getAngle()+(mousePosY - mouseOldY));
-            rotateY.setAngle(rotateY.getAngle()-(mousePosX - mouseOldX));
+            rotateY.setAngle(rotateY.getAngle()-(mouseOldX - mousePosX));
             mouseOldX = mousePosX;
             mouseOldY = mousePosY;
+        });*/
+        scene.setOnKeyPressed(e -> {
+        	if (e.getCode() == KeyCode.R) {
+        		makeRmove(sceneRoot);
+        	} else if (e.getCode() == KeyCode.U) {
+        		makeUmove(sceneRoot);
+        	}
         });
         
         
@@ -128,71 +177,128 @@ public class RubiksCube extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+	private void makeRmove(Group sceneRoot) {
+		for (int x = 0; x < rightFacePoints.size(); x++) {
+        	MeshView msh = (MeshView) sceneRoot.getChildren().get(((x+1)*6)-2);
+        	Point3D pt = rightFacePoints.get(x);
+        	msh.getTransforms().clear();
+        	msh.getTransforms().add(new Translate(pt.getX(), pt.getY(), pt.getZ()));
+        	RotateTransition rt = new RotateTransition(Duration.millis(300), msh);
+        	rt.setAxis(Rotate.X_AXIS);
+        	rt.setByAngle(-90);
+        	rt.setCycleCount(1);
+        	rt.play();
+        	sceneRoot.getChildren().set(((x+1)*6)-2, msh);
+        }
+	}
+	
+	private void makeUmove(Group sceneRoot) {
+		int elem = 0;
+		for (int x = 0; x < upFacePoints.size(); x++) {
+			switch(x) {
+			case 0: elem = 12; break;
+			case 1: elem = 14; break;
+			case 2: elem = 16; break;
+			case 3: elem = 30; break;
+			case 4: elem = 32; break;
+			case 5: elem = 34; break;
+			case 6: elem = 48; break;
+			case 7: elem = 50; break;
+			case 8: elem = 52; break;
+			
+			}
+        	MeshView msh = (MeshView) sceneRoot.getChildren().get(elem);
+        	Point3D pt = upFacePoints.get(x);
+        	msh.getTransforms().clear();
+        	msh.getTransforms().add(new Translate(pt.getX(), pt.getY(), pt.getZ()));
+        	RotateTransition rt = new RotateTransition(Duration.millis(300), msh);
+        	rt.setAxis(Rotate.Y_AXIS);
+        	rt.setByAngle(-90);
+        	rt.setCycleCount(1);
+        	rt.play();
+        	sceneRoot.getChildren().set(elem, msh);
+        }		
+	}
+    
+    //These definitions go "layer by layer", defining the colours for each side of a single piece. 
+    //I'm going to have to add a listener of some sort to each of these points so I can track the colour currently at that location.
+    //I'd imagine these points are fixed and I could rotate the actual blocks without affecting them? Not sure though.
+    
+    //Next task is to figure out how to make these objects rotate together around a centre pivot located in the middle of thats face's centre.
+    
                                               // F   R   U   B   L   D
-    private static final int[] FLD  = new int[]{BLUE, GRAY, GRAY, GRAY, ORANGE, WHITE};
-    private static final int[] FD   = new int[]{BLUE, GRAY, GRAY, GRAY, GRAY, WHITE};
-    private static final int[] FRD  = new int[]{BLUE, RED, GRAY, GRAY, GRAY, WHITE};
-    private static final int[] FL   = new int[]{BLUE, GRAY, GRAY, GRAY, ORANGE, GRAY};
-    private static final int[] F    = new int[]{BLUE, GRAY, GRAY, GRAY, GRAY, GRAY};
-    private static final int[] FR   = new int[]{BLUE, RED, GRAY, GRAY, GRAY, GRAY};
-    private static final int[] FLU  = new int[]{BLUE, GRAY, YELLOW, GRAY, ORANGE, GRAY};
-    private static final int[] FU   = new int[]{BLUE, GRAY, YELLOW, GRAY, GRAY, GRAY};
-    private static final int[] FRU  = new int[]{BLUE, RED, YELLOW, GRAY, GRAY, GRAY};
+    private static final int[] FLD  = new int[]{GREEN, GRAY, GRAY, GRAY, ORANGE, YELLOW};
+    private static final int[] FD   = new int[]{GREEN, GRAY, GRAY, GRAY, GRAY, YELLOW};
+    private static final int[] FRD  = new int[]{GREEN, RED, GRAY, GRAY, GRAY, YELLOW};
+    private static final int[] FL   = new int[]{GREEN, GRAY, GRAY, GRAY, ORANGE, GRAY};
+    private static final int[] F    = new int[]{GREEN, GRAY, GRAY, GRAY, GRAY, GRAY}; //F face centre piece.
+    private static final int[] FR   = new int[]{GREEN, RED, GRAY, GRAY, GRAY, GRAY};
+    private static final int[] FLU  = new int[]{GREEN, GRAY, WHITE, GRAY, ORANGE, GRAY};
+    private static final int[] FU   = new int[]{GREEN, GRAY, WHITE, GRAY, GRAY, GRAY};
+    private static final int[] FRU  = new int[]{GREEN, RED, WHITE, GRAY, GRAY, GRAY};
     
-    private static final Point3D pFLD   = new Point3D(-1.04,  1.04, -1.04);
-    private static final Point3D pFD    = new Point3D(   0,  1.04, -1.04);
-    private static final Point3D pFRD   = new Point3D( 1.04,  1.04, -1.04);
-    private static final Point3D pFL    = new Point3D(-1.04,    0, -1.04);
-    private static final Point3D pF     = new Point3D(   0,    0, -1.04);
-    private static final Point3D pFR    = new Point3D( 1.04,    0, -1.04);
-    private static final Point3D pFLU   = new Point3D(-1.04, -1.04, -1.04);
-    private static final Point3D pFU    = new Point3D(   0, -1.04, -1.04);
-    private static final Point3D pFRU   = new Point3D( 1.04, -1.04, -1.04);
+    private static Point3D pFLD   = new Point3D(-1.04,  1.04, -1.04);
+    private static Point3D pFD    = new Point3D(   0,  1.04, -1.04);
+    private static Point3D pFRD   = new Point3D( 1.04,  1.04, -1.04);
+    private static Point3D pFL    = new Point3D(-1.04,    0, -1.04);
+    private static Point3D pF     = new Point3D(   0,    0, -1.04);
+    private static Point3D pFR    = new Point3D( 1.04,    0, -1.04);
+    private static Point3D pFLU   = new Point3D(-1.04, -1.04, -1.04);
+    private static Point3D pFU    = new Point3D(   0, -1.04, -1.04);
+    private static Point3D pFRU   = new Point3D( 1.04, -1.04, -1.04);
     
-    private static final int[] CLD  = new int[]{GRAY, GRAY, GRAY, GRAY, ORANGE, WHITE};
-    private static final int[] CD   = new int[]{GRAY, GRAY, GRAY, GRAY, GRAY, WHITE};
-    private static final int[] CRD  = new int[]{GRAY, RED, GRAY, GRAY, GRAY, WHITE};
-    private static final int[] CL   = new int[]{GRAY, GRAY, GRAY, GRAY, ORANGE, GRAY};
-    private static final int[] C    = new int[]{GRAY, GRAY, GRAY, GRAY, GRAY, GRAY};
-    private static final int[] CR   = new int[]{GRAY, RED, GRAY, GRAY, GRAY, GRAY};
-    private static final int[] CLU  = new int[]{GRAY, GRAY, YELLOW, GRAY, ORANGE, GRAY};
-    private static final int[] CU   = new int[]{GRAY, GRAY, YELLOW, GRAY, GRAY, GRAY};
-    private static final int[] CRU  = new int[]{GRAY, RED, YELLOW, GRAY, GRAY, GRAY};
+    private static final int[] CLD  = new int[]{GRAY, GRAY, GRAY, GRAY, ORANGE, YELLOW};
+    private static final int[] CD   = new int[]{GRAY, GRAY, GRAY, GRAY, GRAY, YELLOW}; //D face centre piece
+    private static final int[] CRD  = new int[]{GRAY, RED, GRAY, GRAY, GRAY, YELLOW};
+    private static final int[] CL   = new int[]{GRAY, GRAY, GRAY, GRAY, ORANGE, GRAY}; //L face centre piece
+    private static final int[] C    = new int[]{GRAY, GRAY, GRAY, GRAY, GRAY, GRAY}; //Invisible core
+    private static final int[] CR   = new int[]{GRAY, RED, GRAY, GRAY, GRAY, GRAY}; //R face centre piece
+    private static final int[] CLU  = new int[]{GRAY, GRAY, WHITE, GRAY, ORANGE, GRAY};
+    private static final int[] CU   = new int[]{GRAY, GRAY, WHITE, GRAY, GRAY, GRAY}; //U face centre piece
+    private static final int[] CRU  = new int[]{GRAY, RED, WHITE, GRAY, GRAY, GRAY};
     
-    private static final Point3D pCLD   = new Point3D(-1.04,  1.04, 0);
-    private static final Point3D pCD    = new Point3D(   0,  1.04, 0);
-    private static final Point3D pCRD   = new Point3D( 1.04,  1.04, 0);
-    private static final Point3D pCL    = new Point3D(-1.04,    0, 0);
-    private static final Point3D pC     = new Point3D(   0,    0, 0);
-    private static final Point3D pCR    = new Point3D( 1.04,    0, 0);
-    private static final Point3D pCLU   = new Point3D(-1.04, -1.04, 0);
-    private static final Point3D pCU    = new Point3D(   0, -1.04, 0);
-    private static final Point3D pCRU   = new Point3D( 1.04, -1.04, 0);
+    private static Point3D pCLD   = new Point3D(-1.04,  1.04, 0);
+    private static Point3D pCD    = new Point3D(   0,  1.04, 0);
+    private static Point3D pCRD   = new Point3D( 1.04,  1.04, 0);
+    private static Point3D pCL    = new Point3D(-1.04,    0, 0);
+    private static Point3D pC     = new Point3D(   0,    0, 0);
+    private static Point3D pCR    = new Point3D( 1.04,    0, 0);
+    private static Point3D pCLU   = new Point3D(-1.04, -1.04, 0);
+    private static Point3D pCU    = new Point3D(   0, -1.04, 0);
+    private static Point3D pCRU   = new Point3D( 1.04, -1.04, 0);
     
-    private static final int[] BLD  = new int[]{GRAY, GRAY, GRAY, GREEN, ORANGE, WHITE};
-    private static final int[] BD   = new int[]{GRAY, GRAY, GRAY, GREEN, GRAY, WHITE};
-    private static final int[] BRD  = new int[]{GRAY, RED, GRAY, GREEN, GRAY, WHITE};
-    private static final int[] BL   = new int[]{GRAY, GRAY, GRAY, GREEN, ORANGE, GRAY};
-    private static final int[] B    = new int[]{GRAY, GRAY, GRAY, GREEN, GRAY, GRAY};
-    private static final int[] BR   = new int[]{GRAY, RED, GRAY, GREEN, GRAY, GRAY};
-    private static final int[] BLU  = new int[]{GRAY, GRAY, YELLOW, GREEN, ORANGE, GRAY};
-    private static final int[] BU   = new int[]{GRAY, GRAY, YELLOW, GREEN, GRAY, GRAY};
-    private static final int[] BRU  = new int[]{GRAY, RED, YELLOW, GREEN, GRAY, GRAY};
+    private static final int[] BLD  = new int[]{GRAY, GRAY, GRAY, BLUE, ORANGE, YELLOW};
+    private static final int[] BD   = new int[]{GRAY, GRAY, GRAY, BLUE, GRAY, YELLOW};
+    private static final int[] BRD  = new int[]{GRAY, RED, GRAY, BLUE, GRAY, YELLOW};
+    private static final int[] BL   = new int[]{GRAY, GRAY, GRAY, BLUE, ORANGE, GRAY};
+    private static final int[] B    = new int[]{GRAY, GRAY, GRAY, BLUE, GRAY, GRAY}; //B face centre piece
+    private static final int[] BR   = new int[]{GRAY, RED, GRAY, BLUE, GRAY, GRAY};
+    private static final int[] BLU  = new int[]{GRAY, GRAY, WHITE, BLUE, ORANGE, GRAY};
+    private static final int[] BU   = new int[]{GRAY, GRAY, WHITE, BLUE, GRAY, GRAY};
+    private static final int[] BRU  = new int[]{GRAY, RED, WHITE, BLUE, GRAY, GRAY};
     
-    private static final Point3D pBLD   = new Point3D(-1.04,  1.04, 1.04);
-    private static final Point3D pBD    = new Point3D(   0,  1.04, 1.04);
-    private static final Point3D pBRD   = new Point3D( 1.04,  1.04, 1.04);
-    private static final Point3D pBL    = new Point3D(-1.04,    0, 1.04);
-    private static final Point3D pB     = new Point3D(   0,    0, 1.04);
-    private static final Point3D pBR    = new Point3D( 1.04,    0, 1.04);
-    private static final Point3D pBLU   = new Point3D(-1.04, -1.04, 1.04);
-    private static final Point3D pBU    = new Point3D(   0, -1.04, 1.04);
-    private static final Point3D pBRU   = new Point3D( 1.04, -1.04, 1.04);
+    private static Point3D pBLD   = new Point3D(-1.04,  1.04, 1.04);
+    private static Point3D pBD    = new Point3D(   0,  1.04, 1.04);
+    private static Point3D pBRD   = new Point3D( 1.04,  1.04, 1.04);
+    private static Point3D pBL    = new Point3D(-1.04,    0, 1.04);
+    private static Point3D pB     = new Point3D(   0,    0, 1.04);
+    private static Point3D pBR    = new Point3D( 1.04,    0, 1.04);
+    private static Point3D pBLU   = new Point3D(-1.04, -1.04, 1.04);
+    private static Point3D pBU    = new Point3D(   0, -1.04, 1.04);
+    private static Point3D pBRU   = new Point3D( 1.04, -1.04, 1.04);
     
-    private static final List<int[]> patternFaceF = Arrays.asList(
+    private static final List<int[]> patternFaceF = Arrays.asList(    		
             FLD, FD, FRD, FL, F, FR, FLU, FU, FRU,
             CLD, CD, CRD, CL, C, CR, CLU, CU, CRU,
             BLD, BD, BRD, BL, B, BR, BLU, BU, BRU);
+    
+
+    private static final List<Point3D> rightFacePoints = Arrays.asList(
+    		pFRD, pFR, pFRU, pCRD, pCR, pCRU, pBRD, pBR, pBRU);
+    
+    private static final List<Point3D> upFacePoints = Arrays.asList(
+    		pFLU, pFU, pFRU, pCLU, pCU, pCRU, pBLU, pBU, pBRU);
     
     private static final List<Point3D> pointsFaceF = Arrays.asList(
             pFLD, pFD, pFRD, pFL, pF, pFR, pFLU, pFU, pFRU,
